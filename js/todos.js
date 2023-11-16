928c4b2be8f1e3cd956ed95d2e26c5a9002f73e9
shrfrn <shrfrn@gmail.com>
2023-11-16 09:57:58 +0200

    Add toggleTodo()

--- js/todos.js
+++ js/todos.js
@@ -13,18 +13,27 @@ function onInit() {
 function renderTodos() {
     const elTodoList = document.querySelector('.todo-list')
     const strHtml = gTodos.map(todo => `
-        <li>
+        <li onclick="onToggleTodo('${todo.id}')">
             <span class="${ todo.isDone ? 'done' : '' }">${todo.txt}</span>
-            <button onclick="onRemoveTodo('${todo.id}')">x</button>
+            <button onclick="onRemoveTodo(event, '${todo.id}')">x</button>
         </li>
     `).join('')
 
     elTodoList.innerHTML = strHtml
 }
 
-function onRemoveTodo(todoId) {
+function onRemoveTodo(ev, todoId) {
+    ev.stopPropagation()
+
     const idx = gTodos.findIndex(todo => todo.id === todoId)
     gTodos.splice(idx, 1)
+
+    renderTodos()
+}
+
+function onToggleTodo(todoId) {
+    const todo = gTodos.find(todo => todo.id === todoId)
+    todo.isDone = !todo.isDone
 
     renderTodos()
 }
