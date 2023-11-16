bb789a5a71c766d5ae1d75132dbba1f44a90f0da
UriShilo <urishilo360@gmail.com>
2023-11-16 15:59:37 +0200

    add txt / created / importance filter options

--- js/services/todo.service.js
+++ js/services/todo.service.js
@@ -6,10 +6,39 @@ var gFilterBy = 'All'
 _createTodos()
 
 function getTodos() {
-	if (gFilterBy === 'All') return gTodos
+	switch (gFilterBy) {
+		case 'All':
+			return gTodos
 
-	const isDone = gFilterBy === 'Done' ? true : false
-	return gTodos.filter(todo => todo.isDone === isDone)
+		case 'Done':
+		case 'Active':
+			const isDone = gFilterBy === 'Done' ? true : false
+			return gTodos.filter(todo => todo.isDone === isDone)
+
+		case 'Task':
+			return gTodos.sort((todoA, todoB) => {
+				const txtA = todoA.txt.toUpperCase();
+				const txtB = todoB.txt.toUpperCase();
+				if (txtA < txtB) {
+					return -1;
+				}
+				if (txtA > txtB) {
+					return 1;
+				}
+
+				// names must be equal
+				return 0;
+			})
+
+		case 'Created At':
+			return gTodos.sort((todoA, todoB) => todoA.createdAt - todoB.createdAt)
+
+		case 'Importance':
+			return gTodos.sort((todoA, todoB) => todoA.importance - todoB.importance)
+
+		default:
+			return null
+	}
 }
 
 function getTotalCount() {
