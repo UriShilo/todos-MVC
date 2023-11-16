5a5b9b5c79593d4892904fb7f0f9f083313d1744
shrfrn <shrfrn@gmail.com>
2023-11-16 11:26:01 +0200

    Separate util fumctions into a util service

--- js/services/todo.service.js
+++ js/services/todo.service.js
@@ -53,19 +53,3 @@ function _createTodo(txt) {
 	}
 }
 
-function makeId(length = 6) {
-	var id = ''
-	var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-
-	for (var i = 0; i < length; i++) {
-		id += possible.charAt(getRandomInt(0, possible.length))
-	}
-
-	return id
-}
-
-function getRandomInt(min, max) {
-	min = Math.ceil(min)
-	max = Math.floor(max)
-	return Math.floor(Math.random() * (max - min) + min) // The maximum is exclusive and the minimum is inclusive
-}
