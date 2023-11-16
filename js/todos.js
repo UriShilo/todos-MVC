05882e5f84f3fc4dd717ddbb9554f71abbf7e6a6
shrfrn <shrfrn@gmail.com>
2023-11-16 09:41:17 +0200

    Add a data model and render it

--- js/todos.js
+++ js/todos.js
@@ -1,21 +1,27 @@
 'use strict'
 
+var gTodos = [
+    { id: 't101', txt: 'Do this', isDone: false },
+    { id: 't102', txt: 'Do that', isDone: true },
+    { id: 't103', txt: 'Try here', isDone: false },
+]
+
 function onInit() {
     renderTodos()
 }
 
 function renderTodos() {
     const elTodoList = document.querySelector('.todo-list')
-    const strHtml = `
+    const strHtml = gTodos.map(todo => `
         <li>
-            <span class="done">Do this</span>
-            <button onclick="onRemoveTodo(123)">x</button>
+            <span class="${ todo.isDone ? 'done' : '' }">${todo.txt}</span>
+            <button onclick="onRemoveTodo('${todo.id}')">x</button>
         </li>
-    `
+    `).join('')
 
     elTodoList.innerHTML = strHtml
 }
 
 function onRemoveTodo(todoId) {
-    console.log('Hi')
+    console.log(todoId)
 }
