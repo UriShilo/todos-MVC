2737a99977e1196576494a0b521b6bdf144923d2
shrfrn <shrfrn@gmail.com>
2023-11-16 12:19:33 +0200

    Add local storage support with service

--- js/services/todo.service.js
+++ js/services/todo.service.js
@@ -27,22 +27,32 @@ function setFilterBy(filterBy) {
 function addTodo(txt) {
 	const todo = _createTodo(txt)
 	gTodos.unshift(todo)
+
+    _saveTodos()
 }
 
 function removeTodo(todoId) {
 	const idx = gTodos.findIndex(todo => todo.id === todoId)
 	gTodos.splice(idx, 1)
+
+    _saveTodos()
 }
 
 function toggleTodo(todoId) {
 	const todo = gTodos.find(todo => todo.id === todoId)
 	todo.isDone = !todo.isDone
+
+    _saveTodos()
 }
 
 // Private functions
 
 function _createTodos() {
+    gTodos = loadFromStorage('todosDB')
+    if(gTodos && gTodos.length) return
+
 	gTodos = [_createTodo('Do this'), _createTodo('Do that'), _createTodo('Try here')]
+    _saveTodos()
 }
 
 function _createTodo(txt) {
@@ -53,3 +63,6 @@ function _createTodo(txt) {
 	}
 }
 
+function _saveTodos() {
+    saveToStorage('todosDB', gTodos)
+}
